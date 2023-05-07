f18bb5765187bf8105f23f240fe3b8fce7d7d19d
sarthkdobriyal <dobriyalsarthaksa.8@gmail.com>
2023-05-07 22:36:48 +0530

    grestarting integration of next auth

--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,27 @@
-import '../styles/globals.css'
-import Navbar from '@components/Navbar';
+import "../styles/globals.css";
+import Navbar from "@components/Navbar";
+import Provider from "@components/Provider";
 
 export const metadata = {
-  title: 'Promptopedia',
-  description: 'Discover and Share all your chatgpt prompts',
-}
+  title: "Promptopedia",
+  description: "Discover and Share all your chatgpt prompts",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <title>promptopedia</title>
       <body>
-      <div className='main'>
-          <div className='gradient' />
-        </div>
-        <main className='app'>
-          <Navbar />
-          {children}
-        </main>
-        </body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Navbar />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
+  );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,25 @@
 "use client";
 
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
+import { useEffect, useState } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import Image from "next/image";
 
 const Navbar = () => {
-  const isUser = true;
+  const { data: session } = useSession();
 
   const [toggleDropDown, setToggleDropDown] = useState(false);
+
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setupProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+    setupProviders();
+  }, []);
 
   return (
     <nav className="flex py-4 justify-between px-4 border-b border-emerald-300 border-opacity-50 w-full">
@@ -25,51 +38,76 @@ const Navbar = () => {
 
       {/* desktop */}
 
-      {isUser ? (
+      {session?.user ? (
         <div className=" hidden md:flex gap-4 items-center">
           {/* create post button */}
-          <button className="black_btn">Create Prompt</button>
-          <div className="border border-red-500 rounded-full w-5 h-5">
-            {/* User profile photo */}
-          </div>
+          <Link href="/create-prompt" className="black_btn">
+            Create Prompt
+          </Link>
+          <button type="button" onClick={signOut} className="outline_btn">
+            Sign Out
+          </button>
+          <Link href="/profile">
+            <Image
+              src={session?.user.image}
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="profile"
+            />
+          </Link>
         </div>
       ) : (
-        <div className=" hidden md:flex items-center">
-          <button className="black_btn">Sign In</button>
-        </div>
+        <>
+          {providers &&
+            Object.values(providers).map((provider) => (
+              <button
+                type="button"
+                key={provider.name}
+                onClick={() => {
+                  signIn(provider.id);
+                }}
+                className="black_btn"
+              >
+                Sign in
+              </button>
+            ))}
+        </>
       )}
 
       {/* mobile */}
+      <div className="sm:hidden flex relative">
+        {session?.user ? (
+          <div className="flex">
+            <Image
+              src={session?.user.image}
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="profile"
+              onClick={() => setToggleDropdown(!toggleDropdown)}
+            />
 
-      {isUser ? (
-        <div className="md:hidden flex relative">
-          <div className="border border-red-500 rounded-full w-10 h-10">
-            {/* User profile photo */}
-            <div
-              onClick={() => setToggleDropDown((prev) => !prev)}
-              className="h-10 w-10"
-            ></div>
-
-            {toggleDropDown && (
+            {toggleDropdown && (
               <div className="dropdown">
                 <Link
                   href="/profile"
                   className="dropdown_link"
-                  onClick={() => setToggleDropDown(false)}
+                  onClick={() => setToggleDropdown(false)}
                 >
                   My Profile
                 </Link>
                 <Link
                   href="/create-prompt"
                   className="dropdown_link"
-                  onClick={() => setToggleDropDown(false)}
+                  onClick={() => setToggleDropdown(false)}
                 >
                   Create Prompt
                 </Link>
                 <button
                   type="button"
                   onClick={() => {
-                    setToggleDropDown(false);
+                    setToggleDropdown(false);
                     signOut();
                   }}
                   className="mt-5 w-full black_btn"
@@ -79,12 +117,24 @@ const Navbar = () => {
               </div>
             )}
           </div>
-        </div>
-      ) : (
-        <div className="md:hidden flex ">
-          <button className="black_btn text-sm">Sign In</button>
-        </div>
-      )}
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className="black_btn"
+                >
+                  Sign in
+                </button>
+              ))}
+          </>
+        )}
+      </div>
     </nav>
   );
 };
