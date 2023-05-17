85c1dc5e57c98d5c7d5892a74b4ac0d3ceec2a6f
root <root@DESKTOP-4GUT5LC.localdomain>
2023-05-17 11:53:13 +0530

    my profile page complete

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,54 @@
+import Prompt from '@models/Prompt'
+import connectDb from '@utils/database'
+
+//GET
+export const GET = async (req, { params }) => {
+
+    try{
+        await connectDb()
+        const prompt = await Prompt.findById(params.id).populate("creator")
+        if(!prompt )  return new Response("Prompt Not Found", { status: 404 });
+
+        return new Response(JSON.stringify(prompt), { status: 200 })
+
+    }catch(err){
+        return new Response('Server Error', {status: 500})
+    }
+
+}
+
+
+//PATCH
+export const PATCH = async (req, { params }) => {
+    const { prompt, tag } = await request.json();
+    
+    try{
+        await connectDb()
+        const existingPrompt = await Prompt.findById(params.id);
+        if(!existingPrompt )  return new Response("Prompt Not Found", { status: 404 });
+        
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
+
+        await existingPrompt.save();
+
+        return new Response("Successfully updated the Prompts", { status: 200 });
+
+    }catch(err){
+        return new Response('Server Error', {status: 500})
+    }
+}
+
+//DELETE
+export const DELETE = async (req, { params }) => {
+
+    try{
+        await connectDb()
+        await Prompt.findByIdAndRemove(params.id);
+
+        return new Response("Prompt deleted successfully", { status: 200 });
+
+    }catch(err){
+        return new Response('Server Error', {status: 500})
+    }
+}
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,44 +7,62 @@ import { useRouter } from 'next/navigation'
 
 const MyProfile = () => {
 
-    const {data: session } = useSession();
-    const router = useRouter();
+  const { data: session } = useSession();
+  const router = useRouter();
 
-    const [prompts, setPrompts] = useState([]);
+  const [prompts, setPrompts] = useState([]);
 
-    const handleEdit = () => {
+  const handleEdit = (post) => {
+    router.push('/update-prompt?id=${post._id}')
+  }
+
+  const handleDelete = async (post) => {
+    const confirmed = confirm("Do you really want to delete this prompt. You won't be able to undo")
+    if (confirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+
+        const filteredPosts = myPosts.filter((item) => item._id !== post._id);
+        
+        setPrompts(filteredPosts);
+
+
+
+      } catch (err) {
+        console.log(err)
+      }
 
     }
+  }
 
-    const handleDelete = async () => {
 
-    }
-   
-    
-    const fetchPrompts = async () => {
-        const response = await fetch(`/api/users/${session?.user.id}/posts`)
-        const data = await response.json();
+  const fetchPrompts = async () => {
+    const response = await fetch(`/api/users/${session?.user.id}/posts`)
+    const data = await response.json();
 
-        setPrompts(data);
-    }
+    setPrompts(data);
+  }
 
-    console.log(prompts)
-    useEffect(() => {
-        if (session?.user.id) fetchPrompts();
-    }, [session?.user.id])
+  // console.log(prompts)
+  useEffect(() => {
+    if (session?.user.id) fetchPrompts();
+  }, [session?.user.id])
 
 
 
   return (
     <>
-    
-    <Profile 
+
+      <Profile
         name="My"
         desc="Welcome to your Profile page. All your prompts can be manipulated here"
         data={prompts}
         handleEdit={handleEdit}
         handleDelete={handleDelete}
-        />
+      />
     </>
   )
 }
